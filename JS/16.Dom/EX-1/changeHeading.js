dcd84131b5c4eb007a964eaf435795fe66bc26eb
janakiram <pjram9806@gmail.com>
2025-10-08 14:02:43 +0530

    dom added

---
+++ JS/16.Dom/EX-1/changeHeading.js
@@ -0,0 +1,8 @@
+/* console.log(document.getElementById('headingElement'));
+ */
+
+function manipulateStyles() {
+  document.getElementById("headingElement").textContent =
+    "Latest technologoies";
+  document.getElementById("headingElement").style.color = "blue";
+}
