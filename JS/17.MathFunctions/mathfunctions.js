5f02ec54866d88ce77e640441a2a3c890b1fc5bb
janakiram <pjram9806@gmail.com>
2025-10-09 18:36:49 +0530

    updated

---
+++ JS/17.MathFunctions/mathfunctions.js
@@ -0,0 +1,13 @@
+//console.log(Math.ceil(Math.random()*100));
+
+//console.log(Math.ceil(47.14389181180687));
+let age = 25;
+
+if(age==24)
+{
+    console.log(typeof(age));
+}
+else if(age === 25)
+{
+    console.log(typeof(age))
+}
