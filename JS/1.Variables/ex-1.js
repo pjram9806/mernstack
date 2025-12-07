e9c6376dfcff5e75a0835815bfd90ea6e17725c9
janaki ram <pjram9806gmail.com>
2025-12-07 18:43:16 +0530

    commit

---
+++ JS/1.Variables/ex-1.js
@@ -0,0 +1,7 @@
+let name ='satish',email= 'satish@gmail.com',password='pass@123';
+let mobile = 87894561233;
+let address = 'Hyderabad',pincode=500072;
+let monthlySalary = 25000;
+let annualSalary = 250000;
+
+console.log(name,email,password,mobile,monthlySalary,annualSalary,address,pincode);
---
+++ JS/1.Variables/ex-2.js
@@ -0,0 +1,8 @@
+let productId = 1;
+let title = 'Watch';
+let price = 2500;
+console.log(productId,price,title);
+
+productId = 2,title = 'Raymond Watch',price = '1800';
+console.log(productId,price,title);
+
